3df992ca1a981c3d17ad34e2241ad3346f43a678
Giacomo Annaloro <g.annaloro@reply.it>
2023-04-14 10:03:59 +0200

    Preparing empty playground

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { Observable, subscribeOn } from 'rxjs';
-
-const obeservable$ = new Observable<string>(subscriber => {
-  console.log('Observable executed');
-})
-
-console.log('Before subscribe');
-obeservable$.subscribe();
-console.log('After subscribe');
